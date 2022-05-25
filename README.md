8abc4749cd4577056518041bfc681017cb457302
AleksandrF93 <86001296+AleksandrF93@users.noreply.github.com>
2022-05-25 12:44:08 +0300

    добавляет изменения

---
+++ README.md
@@ -0,0 +1,4 @@
+1- https://monosnap.com/file/9WsZkSIC9oGbP7HXCHFtoFKwNiigT6;
+2- https://monosnap.com/file/jNosvagVP5MzBELni8a62zRmv2VgJI;
+3- https://monosnap.com/file/RzDECTG8fSNkVtFAtH3j0AUPt6uF15;
+4- https://monosnap.com/file/RLeKusKErEtqgcH12NEacswT9qUmEt;
---
+++ index.js
@@ -0,0 +1,31 @@
+const { listContacts, removeContact, getContactById, addContact } = require('./contacts');
+const argv = require('yargs').argv;
+
+const invokeAction = async ({ action, id, name, email, phone })=> {
+  switch (action) {
+    case "list":
+      const contacts = await listContacts();
+      console.table(contacts)
+      break;
+
+    case "get":
+     const getContact =  await getContactById(id);
+     console.log(getContact)
+      break;
+
+    case "add":
+      const contactAdd= await addContact(name, phone,email)
+      console.log(contactAdd)
+      break;
+
+      case "remove":
+        const contactRemove= await removeContact(id)
+        console.log(contactRemove)
+        break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
